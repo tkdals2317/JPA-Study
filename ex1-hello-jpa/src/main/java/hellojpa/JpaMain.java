b06d4d2418fb82cea8045381ffaa585e757fd53f
이상민 <tkdals2317@naver.com>
2021-07-10 23:57:58 +0900

    0710공부

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -43,13 +43,29 @@ public class JpaMain {
 
             //JPQL
             //멤버 전체 가져오기
-            List<Member> result1 = em.createQuery("select m from Member as m", Member.class).getResultList();
+            //List<Member> result1 = em.createQuery("select m from Member as m", Member.class).getResultList();
             //페이징
-            List<Member> result2 = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(5)
-                    .setMaxResults(8)
-                    .getResultList();
-            //
+            //List<Member> result2 = em.createQuery("select m from Member as m", Member.class)
+            //        .setFirstResult(5)
+            //       .setMaxResults(8)
+            //        .getResultList();
+            //영속성 컨텍스트
+            //1. 비영속
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA");
+
+            //2. 영속
+            System.out.println("=== BEFORE ===");
+            em.persist(member);
+
+            //3. 준영속, 삭제
+            //회원 엔티티를 영속성 컨텍스트에서 분리, 준영속 상태
+            em.detach(member);
+            //객체를 삭제한 상태(실제 DB에서 지우는 것)
+            em.remove(member);
+
+            System.out.println("=== AFTER ===");
             //커밋
             tx.commit();
         }catch (Exception e){
